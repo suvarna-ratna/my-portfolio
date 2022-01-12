0a7df7ecb1a33d627dfa5141c8aa79e9f8873c13
suvarna <suvarna.akkala@gmail.com>
2022-01-12 17:48:42 +0100

    update links

--- src/compoenents/Navbar.js
+++ src/compoenents/Navbar.js
@@ -17,10 +17,10 @@ export default function Navbar() {
           <a href="#skills" className="mr-5 hover:text-white">
             Skills
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
+          <a href="#education" className="mr-5 hover:text-white">
             Education
           </a>
-          <a href="#testimonials" className="mr-5 hover:text-white">
+          <a href="#experience" className="mr-5 hover:text-white">
           Work Experience
           </a>
         </nav>
